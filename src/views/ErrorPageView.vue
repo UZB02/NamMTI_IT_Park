b4567eb440e4a304cfb44101bfaf1ff70c987493
UZB02 <muhsinbekmirzamatov3@gmail.com>
2024-12-12 02:31:17 -0800

    new commit

--- src/views/ErrorPageView.vue
+++ src/views/ErrorPageView.vue
@@ -1,9 +1,18 @@
 <template>
-  <section class="flex items-center justify-center h-[85vh] bg-gray-100" :class="isDarkTheme ? 'bg-slate-800' : 'bg-gray-100'">
+  <section
+    class="flex items-center justify-center h-[85vh] bg-gray-100"
+    :class="isDarkTheme ? 'bg-slate-800' : 'bg-gray-100'"
+  >
     <div class="container px-2 text-center">
-      <div class="text-6xl font-bold text-blue-600 animate-bounce relative z-[5]">409</div>
-      <p class="text-2xl mt-4 ">Kechirasiz tez orada qo'shiladi!</p>
-      <p class="mt-2 text-gray-500">Sahifa tez orada ishga tushiriladi. Bizdan uzoqlashmang.</p>
+      <div
+        class="text-6xl font-bold text-blue-600 animate-bounce relative z-[5]"
+      >
+        409
+      </div>
+      <p class="text-2xl mt-4">Kechirasiz tez orada qo'shiladi!</p>
+      <p class="mt-2 text-gray-500">
+        Sahifa tez orada ishga tushiriladi. Bizdan uzoqlashmang.
+      </p>
       <button
         @click="goHome"
         class="mt-6 px-6 py-2 text-white bg-blue-600 rounded hover:bg-blue-700 transition-all duration-300"
@@ -15,14 +24,14 @@
 </template>
 
 <script setup>
-import { useLayout } from '@/layouts/composables/layout';
-import { useRouter } from 'vue-router';
+import { useLayout } from "@/layouts/composables/layout";
+import { useRouter } from "vue-router";
 
 const { isDarkTheme } = useLayout();
 
 const router = useRouter();
 const goHome = () => {
-  router.push('/');
+  router.push("/");
 };
 </script>
 
